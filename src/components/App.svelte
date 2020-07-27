950397b10a0952f106f5a2085f9a20f169610509
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-07-27 14:34:38 +0100

    initial shell

---
+++ src/components/App.svelte
@@ -0,0 +1,140 @@
+<script lang="ts">
+  import MenuToggle from './MenuToggle.svelte'
+
+  export let name: string
+
+  let asideOpen: boolean
+
+  function handleMenuToggle(event: any) {
+    asideOpen = event.detail.menuOpen
+  }
+</script>
+
+<style>
+  .wrapper {
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    grid-template-rows: auto 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    min-height: 100vh;
+  }
+
+  header {
+    position: sticky;
+    top: 0;
+    grid-column: 1 / -1;
+    background-color: var(--primary-dark);
+    color: var(--primary-light);
+    display: flex;
+    min-height: 4rem;
+    align-items: center;
+    padding: 0 1rem;
+    z-index: 2;
+  }
+
+  .content {
+    grid-column: 1/-1;
+    grid-row: 2;
+    display: flex;
+  }
+
+  aside {
+    width: 0;
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.2s;
+    background-color: var(--secondary-light);
+  }
+
+  .aside-content {
+    position: sticky;
+    top: 4rem;
+    padding: 1rem;
+  }
+
+  .asideOpen {
+    visibility: visible;
+    opacity: 1;
+    width: 30rem;
+  }
+
+  main {
+    padding: 1rem;
+    flex: 1;
+  }
+
+  :global(.icon) {
+    padding: 0.5rem;
+  }
+
+  .title {
+    font-size: 1.5rem;
+    vertical-align: middle;
+    margin-left: 1rem;
+  }
+</style>
+
+<div class="wrapper">
+  <header>
+    <MenuToggle on:menuToggle={handleMenuToggle} />
+    <div class="title">Header</div>
+  </header>
+
+  <div class="content">
+    <aside class:asideOpen>
+      <div class="aside-content">Aside</div>
+    </aside>
+
+    <main>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+      <p>Hello {name}</p>
+    </main>
+  </div>
+</div>
---
+++ src/components/MenuToggle.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+  let open = false
+
+  $: dispatch('menuToggle', { menuOpen: open })
+</script>
+
+<style>
+  input[type='checkbox'] {
+    transition: all 0.2s;
+    box-sizing: border-box;
+    display: none;
+  }
+
+  .nav-icon {
+    width: 2rem;
+  }
+
+  .nav-icon:after,
+  .nav-icon:before,
+  .nav-icon div {
+    background-color: #fff;
+    border-radius: 3px;
+    content: '';
+    display: block;
+    height: 5px;
+    margin: 7px 0;
+    transition: all 0.1s ease-in-out;
+  }
+
+  .open.nav-icon:before {
+    transform: translateY(12px) rotate(45deg);
+  }
+
+  .open.nav-icon:after {
+    transform: translateY(-12px) rotate(-45deg);
+  }
+
+  .open.nav-icon div {
+    transform: scale(0);
+  }
+</style>
+
+<div>
+  <input type="checkbox" bind:checked={open} id="openSidebarMenu" />
+  <label for="openSidebarMenu" class="nav-icon" class:open>
+    <div />
+  </label>
+</div>
