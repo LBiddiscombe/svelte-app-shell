e0e35787ec65a16e38fadba860a6b619be1286dd
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-08-01 19:56:27 +0100

    updated filler

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -10,52 +10,178 @@
 </style>
 
 <main>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
-  <p>Content goes here...</p>
+  <h1>But, like most politicians, he promised more than he could deliver.</h1>
+  <p>
+    Wow! A superpowers drug you can just rub onto your skin? You'd think it would be something you'd have to freebase.
+    You, a bobsleder!? That I'd like to see! You can see how I lived before I met you. Kids have names?
+  </p>
+  <p>
+    Have you ever tried just turning off the TV, sitting down with your children, and hitting them?
+    <strong>Michelle, I don't regret this, but I both rue and lament it.</strong>
+    <em>Is the Space Pope reptilian!?</em>
+    Yeah, lots of people did.
+  </p>
+  <h2>I wish! It's a nickel.</h2>
+  <p>
+    Why yes! Thanks for noticing. Large bet on myself in round one. A true inspiration for the children. With gusto.
+  </p>
+  <ol>
+    <li>
+      Wow! A superpowers drug you can just rub onto your skin? You'd think it would be something you'd have to freebase.
+    </li>
+    <li>Noooooo!</li>
+    <li>That's a popular name today. Little "e", big "B"?</li>
+  </ol>
+
+  <h3>Bender, hurry! This fuel's expensive! Also, we're dying!</h3>
+  <p>
+    The key to victory is discipline, and that means a well made bed. You will practice until you can make your bed in
+    your sleep. But I know you in the future. I cleaned your poop. You lived before you met me?!
+  </p>
+  <ul>
+    <li>Pansy.</li>
+    <li>
+      Hey, tell me something. You've got all this money. How come you always dress like you're doing your laundry?
+    </li>
+    <li>
+      When I was first asked to make a film about my nephew, Hubert Farnsworth, I thought "Why should I?" Then later,
+      Leela made the film. But if I did make it, you can bet there would have been more topless women on motorcycles.
+      Roll film!
+    </li>
+  </ul>
+
+  <p>
+    The key to victory is discipline, and that means a well made bed. You will practice until you can make your bed in
+    your sleep. And from now on you're all named Bender Jr. I am Singing Wind, Chief of the Martians.
+  </p>
+  <p>
+    So, how 'bout them Knicks? Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that
+    panders to my lust for violence? Enough about your promiscuous mother, Hermes! We have bigger problems.
+  </p>
+  <p>
+    Look, last night was a mistake. Moving along… Good news, everyone! There's a report on TV with some very bad news!
+    Bender, you risked your life to save me! But existing is basically all I do!
+  </p>
+  <p>
+    Switzerland is small and neutral! We are more like Germany, ambitious and misunderstood! Tell them I hate them. Is
+    today's hectic lifestyle making you tense and impatient? And when we woke up, we had these bodies.
+  </p>
+  <p>
+    Alright, let's mafia things up a bit. Joey, burn down the ship. Clamps, burn down the crew. Yes, if you make it look
+    like an electrical fire. When you do things right, people won't be sure you've done anything at all.
+  </p>
+  <p>
+    Why did you bring us here? Wow! A superpowers drug you can just rub onto your skin? You'd think it would be
+    something you'd have to freebase. I suppose I could part with 'one' and still be feared…
+  </p>
+  <p>
+    Switzerland is small and neutral! We are more like Germany, ambitious and misunderstood! The key to victory is
+    discipline, and that means a well made bed. You will practice until you can make your bed in your sleep.
+  </p>
+  <p>
+    You can crush me but you can't crush my spirit! Bender, quit destroying the universe! No, just a regular mistake. Of
+    all the friends I've had… you're the first. We'll go deliver this crate like professionals, and then we'll go home.
+  </p>
+  <p>
+    Switzerland is small and neutral! We are more like Germany, ambitious and misunderstood! Fry! Stay back! He's too
+    powerful! Really?! Negative, bossy meat creature! One hundred dollars.
+  </p>
+  <p>
+    You'll have all the Slurm you can drink when you're partying with Slurms McKenzie! In our darkest hour, we can stand
+    erect, with proud upthrust bosoms. That's the ONLY thing about being a slave. I guess because my parents keep
+    telling me to be more ladylike. As though!
+  </p>
+  <p>
+    Tell her she looks thin. Oh, but you can. But you may have to metaphorically make a deal with the devil. And by
+    "devil", I mean Robot Devil. And by "metaphorically", I mean get your coat. I guess if you want children beaten, you
+    have to do it yourself.
+  </p>
+
+  <p>
+    We'll need to have a look inside you with this camera. Of all the friends I've had… you're the first. Uh, is the
+    puppy mechanical in any way? Of all the friends I've had… you're the first.
+  </p>
+  <p>
+    Bender, we're trying our best. You guys go on without me! I'm going to go… look for more stuff to steal!
+    <strong>Are you crazy?</strong>
+    <em>I can't swallow that.</em>
+    Maybe I love you so much I love you no matter who you are pretending to be.
+  </p>
+  <h2>Hello, little man. I will destroy you!</h2>
+  <p>
+    Shinier than yours, meatbag. A sexy mistake. Hi, I'm a naughty nurse, and I really need someone to talk to. $9.95 a
+    minute. So I really am important? How I feel when I'm drunk is correct? Ok, we'll go deliver this crate like
+    professionals, and then we'll go ride the bumper cars.
+  </p>
+  <ol>
+    <li>I daresay that Fry has discovered the smelliest object in the known universe!</li>
+    <li>Good man. Nixon's pro-war and pro-family.</li>
+    <li>I can explain. It's very valuable.</li>
+  </ol>
+
+  <h3>Your best is an idiot!</h3>
+  <p>
+    No! The cat shelter's on to me. I am Singing Wind, Chief of the Martians. Oh yeah, good luck with that. This opera's
+    as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel.
+    That makes me feel angry!
+  </p>
+  <ul>
+    <li>Stop! Don't shoot fire stick in space canoe! Cause explosive decompression!</li>
+    <li>When will that be?</li>
+    <li>Wow, you got that off the Internet? In my day, the Internet was only used to download pornography.</li>
+  </ul>
+
+  <p>
+    Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! I love you, buddy! When will that
+    be? Humans dating robots is sick. You people wonder why I'm still single? It's 'cause all the fine robot sisters are
+    dating humans!
+  </p>
+  <p>
+    Oh Leela! You're the only person I could turn to; you're the only person who ever loved me. You know, I was God
+    once. You know, I was God once. OK, if everyone's finished being stupid. Quite possible. We live long and are
+    celebrated poopers.
+  </p>
+  <p>
+    I guess if you want children beaten, you have to do it yourself. Yep, I remember. They came in last at the Olympics,
+    then retired to promote alcoholic beverages! I just want to talk. It has nothing to do with mating. Fry, that
+    doesn't make sense.
+  </p>
+  <p>
+    Now, now. Perfectly symmetrical violence never solved anything. No, just a regular mistake. Fetal stemcells, aren't
+    those controversial? Humans dating robots is sick. You people wonder why I'm still single? It's 'cause all the fine
+    robot sisters are dating humans!
+  </p>
+  <p>
+    You guys go on without me! I'm going to go… look for more stuff to steal! We'll need to have a look inside you with
+    this camera. We'll go deliver this crate like professionals, and then we'll go home.
+  </p>
+  <p>
+    Yeah, lots of people did. Yes! In your face, Gandhi! Look, last night was a mistake. I could if you hadn't turned on
+    the light and shut off my stereo.
+  </p>
+  <p>
+    Fatal. But I know you in the future. I cleaned your poop. We need rest. The spirit is willing, but the flesh is
+    spongy and bruised. The alien mothership is in orbit here. If we can hit that bullseye, the rest of the dominoes
+    will fall like a house of cards. Checkmate.
+  </p>
+  <p>
+    Quite possible. We live long and are celebrated poopers. Oh, how I wish I could believe or understand that! There's
+    only one reasonable course of action now: kill Flexo! Bender! Ship! Stop bickering or I'm going to come back there
+    and change your opinions manually!
+  </p>
+  <p>
+    Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! Hi, I'm a naughty nurse, and I
+    really need someone to talk to. $9.95 a minute. Aww, it's true. I've been hiding it for so long.
+  </p>
+  <p>
+    Well, then good news! It's a suppository. And I'm his friend Jesus. The key to victory is discipline, and that means
+    a well made bed. You will practice until you can make your bed in your sleep. Tell her you just want to talk. It has
+    nothing to do with mating.
+  </p>
+  <p>
+    Oh right. I forgot about the battle. Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me
+    with my sexual inhibitions? You don't know how to do any of those. When I was first asked to make a film about my
+    nephew, Hubert Farnsworth, I thought "Why should I?" Then later, Leela made the film. But if I did make it, you can
+    bet there would have been more topless women on motorcycles. Roll film!
+  </p>
 </main>
